196538f73d2860edaacd08516e80e5c10af6f6d8
negiashish1 <113715890+negiashish1@users.noreply.github.com>
2023-02-04 02:42:01 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ import Footer from './components/Footer';
 import Gist from './components/Gist';
 import Header from './components/Header';
 import Headervideo from './components/Headervideo';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
+      <Blog/>
       <Navbar/>
       <Header/>
       <Headervideo/>
@@ -24,6 +28,8 @@ function App() {
           <Route path="/login" element={<Services/>} />
           <Route path="/signup" element={<Services/>} />
           <Route path="/about" element={<Services/>} />
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/blog/:id" element={<Blog/>}/>
         </Routes>
       </BrowserRouter>
     </div>
