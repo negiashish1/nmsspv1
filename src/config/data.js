282b0d18d95dac7d50f5fb9529b5078d7303e5ad
negiashish1 <negi.ashish012@gmail.com>
2023-12-26 01:15:26 +0530

    rt

--- src/config/data.js
+++ src/config/data.js
@@ -1,24 +1,24 @@
 export const blogList = [
-  {
-    id: 1,
-    title: 'Krishna Janamashtmi',
-    category: 'culture',
-    subCategory: ['frontend', 'ui/ux', 'design'],
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    authorName: 'Santoshi Madwal',
-    //authorAvatar: '/assets/images/author.jpg',
-    authorAvatar: '/assets/images/Santoshi1.jpg',
-    createdAt: 'September 07, 2023',
-    cover: '/assets/images/krish_jan.jpg',
-  },
+  // {
+  //   id: 1,
+  //   title: 'Krishna Janamashtmi',
+  //   category: 'culture',
+  //   subCategory: ['frontend', 'ui/ux', 'design'],
+  //   description:
+  //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  //   authorName: 'Santoshi Madwal',
+  //   //authorAvatar: '/assets/images/author.jpg',
+  //   authorAvatar: '/assets/images/Santoshi1.jpg',
+  //   createdAt: 'September 07, 2023',
+  //   cover: '/assets/images/krish_jan.jpg',
+  // },
   {
     id: 2,
-    title: 'Milan Places That Highlight The City',
+    title: 'सांस्कृतिक समूह यात्रा',
     category: 'travel',
     subCategory: ['vacation', 'holidays', 'sight seeing'],
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "साथ में सफलता की ऊँचाइयों की ओर एक कदम। सांस्कृतिक सफर में आत्मा को खोजें और नेतृत्व की दिशा में अग्रणी बनें। हर कदम पर सकीख, हर मुसीबत में समर्पण। समृद्धि का रास्ता एक साथी समूह में छिपा होता है, जो साझा करता है और बढ़ता है।",
     authorName: 'Santoshi Madwal',
     authorAvatar: '/assets/images/Santoshi1.jpg',
     createdAt: 'June 03, 2021',
@@ -26,11 +26,11 @@ export const blogList = [
   },
   {
     id: 3,
-    title: 'Online Shopping – An Alternative to Shopping in the Mall',
-    category: 'shopping',
+    title: 'शिक्षा का महत्व',
+    category: 'learning',
     subCategory: ['e-commerce store', 'clothing', 'shopping store'],
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "शिक्षा वंचित वर्ग के लिए अत्यंत महत्वपूर्ण है। यह समाज में समरसता बढ़ाती है और अवसरों का समान वितरण सुनिश्चित करती है। शिक्षा से उन्हें अपने अधिकारों का ज्ञान होता है, जो उन्हें स्वतंत्रता और आत्मनिर्भरता में मदद करता है। इससे उनका जीवनस्तर सुधारता है और समृद्धि की दिशा में कदम बढ़ता है।",
     authorName: 'Santoshi Madwal',
     authorAvatar: '/assets/images/Santoshi1.jpg',
     createdAt: 'June 03, 2021',
@@ -62,11 +62,11 @@ export const blogList = [
   },
   {
     id: 6,
-    title: 'Beyond the Beach',
-    category: 'travel',
+    title: 'कुमाऊं और गढ़वाली शिक्षा कक्षाएं',
+    category: 'education',
     subCategory: ['beaches', 'sea', 'holidays'],
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "कुमाऊं और गढ़वाली शिक्षा कक्षाएं: भूमि के सांस्कृतिक धरोहर को समर्पित। यहाँ भाषा के सौंदर्य में रंगीनी, साहित्यिक समृद्धि का स्रोत है। हमारी भूमि की शानदारी को समझने के लिए आइए, साथ में बढ़ाएं और बचाएं इस सांस्कृतिक विरासत को।",
     authorName: 'Santoshi Madwal',
     authorAvatar: '/assets/images/Santoshi1.jpg',
     createdAt: 'June 03, 2021',
